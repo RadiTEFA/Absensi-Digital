6e06818ce28cc4f015a99eea8822fa4ca8e0a8f0
RadiTEFA <radingoding@gmail.com>
2024-10-09 10:20:43 +0700

    Delete pages directory

--- pages/absensi/tambah.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-12">
-        <h2 class="text-center my-4">FORM ABSENSI</h2>
-      </div>
-    </div>
-  </div>
-</template>
